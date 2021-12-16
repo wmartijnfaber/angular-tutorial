d9723ba543b770364e109898b26847c282157381
Denis Prins <d.prins@das.nl>
2021-12-16 16:08:04 +0100

    tutorial done!

--- src/app/state/check.state.ts
+++ src/app/state/check.state.ts
@@ -1,45 +1,50 @@
 import { Injectable } from '@angular/core';
-import {Action, Selector, State, StateContext} from '@ngxs/store';
-
+import { Action, Selector, State, StateContext } from '@ngxs/store';
 
 /// ACTION
-export class UpdateCheck {
+export class UpdateCheckStates
+{
   static readonly type = '[Module] Fill';
-  constructor(public  payload: CheckStateModel) {
+  constructor(public  payload: CheckStateModel)
+  {
   }
 }
 
-
-export interface CheckStateModel {
+export interface CheckStateModel
+{
   firstCheck: boolean,
   secondCheck: boolean,
-  thirdCheck: boolean,
+  thirdCheck: boolean
 }
 
-@State<CheckStateModel>({
+@State<CheckStateModel>(
+{
   name: 'modules',
-  defaults: {
+  defaults:
+  {
       firstCheck: false,
       secondCheck: false,
-      thirdCheck: false,
+      thirdCheck: false
   }
 })
-@Injectable()
-export class CheckState {
 
+@Injectable()
+export class CheckState
+{
   @Selector()
-  static getThirdCheck(state: CheckStateModel): boolean {
+  static getThirdCheck(state: CheckStateModel): boolean
+  {
     return state.thirdCheck;
   }
 
-  @Action(UpdateCheck)
-  fillList(ctx: StateContext<CheckStateModel>, action: any) {
-    ctx.patchState({
+  @Action(UpdateCheckStates)
+  fillList(ctx: StateContext<CheckStateModel>, action: any): void
+  {
+    ctx.patchState(
+    {
       firstCheck: action.payload.firstCheck,
       secondCheck: action.payload.secondCheck,
       thirdCheck: action.payload.thirdCheck
     })
   }
-
-
 }
